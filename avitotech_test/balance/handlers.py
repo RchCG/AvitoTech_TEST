59ccd395eff7bc2dc349790c4dea09ec2f4e6341
RchCG <RchChuG@gmail.com>
2023-10-09 21:06:39 +0300

    Added Exception handling for routers

--- avitotech_test/balance/handlers.py
+++ avitotech_test/balance/handlers.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from avitotech_test.balance.router import register_new_user, replenishment, charge_off, checking_balance, transfer_money
@@ -11,19 +11,32 @@ balance_router = APIRouter(prefix="/api/v1/balance", tags=["UserBalance"])
 
 @balance_router.post("/create_user", tags=["UserBalance"], summary="Создание пользователя", status_code=201)
 async def create_user(user_id: UUID = None, db: AsyncSession = Depends(get_db)):
-    return await register_new_user(user_id, db)
+    query = await register_new_user(user_id, db)
+
+    if query is False:
+        raise HTTPException(status_code=404, detail="Something went wrong, user is not created!")
+
+    return query
 
 
 @balance_router.post("/enrollment", tags=["UserBalance"], summary="Зачисление средств", status_code=200)
 async def enrollment(user_id: UUID, balance_id: UUID, amount: float, db: AsyncSession = Depends(get_db)):
     query = await replenishment(user_id=user_id, balance_id=balance_id, amount=amount, db=db)
-    return {f"Баланс счёта {balance_id} пополнен на: {amount} RUB"}
+
+    if query is False:
+        raise HTTPException(status_code=404, detail="Something went wrong, enrollment failed")
+
+    return {f"The user balance was enrolled by {amount}"}
 
 
 @balance_router.post("/write_off", tags=["UserBalance"], summary="Списание средств", status_code=200)
 async def write_off(user_id: UUID, balance_id: UUID, amount: float, db: AsyncSession = Depends(get_db)):
     query = await charge_off(user_id=user_id, balance_id=balance_id, amount=amount, db=db)
-    return {f"C баланса счёта {balance_id} списано: {amount} RUB"}
+
+    if query is False:
+        raise HTTPException(status_code=404, detail="Something went wrong, write off failed")
+
+    return {f"{amount} were debited from the balance({balance_id})."}
 
 
 @balance_router.post("/remittance", tags=["UserBalance"], summary="Перевод средств другому клиенту", status_code=200)
@@ -32,10 +45,18 @@ async def remittance(sender_id: UUID, sender_balance_id: UUID, recipient_id: UUI
     query = await transfer_money(sender_id=sender_id, sender_balance_id=sender_balance_id,
                                  recipient_id=recipient_id,
                                  recipient_balance_id=recipient_balance_id, amount=amount, db=db)
-    return {f"Перевод с кошелька {sender_balance_id} на {recipient_balance_id} произведён успешно"}
+
+    if query is False:
+        raise HTTPException(status_code=404, detail="Something went wrong, remittance failed")
+
+    return {f"remittance from balance {sender_balance_id} to {recipient_balance_id} was successful"}
 
 
 @balance_router.get("/current_balance", tags=["UserBalance"], summary="Просмотреть текущий баланс", status_code=200)
 async def current_balance(user_id: UUID, balance_id: UUID, db: AsyncSession = Depends(get_db)):
     query = await checking_balance(user_id=user_id, balance_id=balance_id, db=db)
-    return {f"Текущий баланс составляет {query}"}
+
+    if query is False:
+        raise HTTPException(status_code=404, detail="Something went wrong")
+
+    return {f"Your current balance: {query}"}
