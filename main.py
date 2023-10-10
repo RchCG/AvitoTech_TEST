f806d16395b9415b3c6e88c49e39c534a718cff0
RchCG <RchChuG@gmail.com>
2023-10-10 15:11:50 +0300

    Small hotfix

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
-from avitotech_test.balance.handlers import balance_router
+from avitotech_test.api.balance.handlers import balance_router
 
-app = FastAPI(title="AvitoTech_TEST")
 
-# set routes to the app instance
-app.include_router(balance_router)
+def include_router(app):  # set routes to the app instance
+    app.include_router(balance_router)
+
+
+def start_application():  # start config of the app and including routers to it
+    app = FastAPI(title="AvitoTech_TEST", version="0.0.3")
+    include_router(app)
+
+    return app
+
 
 if __name__ == '__main__':
     # run app on the host and port
-    uvicorn.run(app, host="localhost", port=8080)
+    app = start_application()
+    uvicorn.run(app, host='0.0.0.0', port=8001)
