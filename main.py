0a80b8ab32803bd96bd67763404ff8400341a292
RchCG <RchChuG@gmail.com>
2023-10-12 16:00:09 +0300

    Little hotfix

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def include_router(app):  # set routes to the app instance
     app.include_router(balance_router)
 
 
-def start_application():  # start config of the app and including routers to it
-    app = FastAPI(title="AvitoTech_TEST", version="0.0.3")
-    include_router(app)
+app = FastAPI(title="AvitoTech_TEST", version="0.0.3")
+include_router(app)
 
-    return app
+
+@app.get("/")
+async def index():
+    return "add /docs to your current url to see all features of the app"
 
 
 if __name__ == '__main__':
-    # run app on the host and port
-    app = start_application()
-    uvicorn.run(app, host='0.0.0.0', port=8001)
+    uvicorn.run(app, host='0.0.0.0', port=8000)  # run app on the host and port
